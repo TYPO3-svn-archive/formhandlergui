365bb352d2e2002fec60396e0473a5198ef40719
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-28 21:31:46 +0000

    Related to #4346,#4388 - Works fine when formhandler plugin is not configured by static template but with addPiST43 in ext_tables.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandlergui/trunk@23756 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -27,16 +27,5 @@ if (TYPO3_MODE == 'BE') {
 	t3lib_extMgm::addModule('tools', 'txformhandlerguiM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Controller/Module/');
 }
 
-/* Throw formhandler static template out of TCA
-if (is_array($TCA['sys_template']['columns']['include_static_file']['config']['items'])) {
-	foreach($TCA['sys_template']['columns']['include_static_file']['config']['items'] as $i => $a) {
-		if ($a[1]=='EXT:formhandler/Configuration/Settings/')
-			unset($TCA['sys_template']['columns']['include_static_file']['config']['items'][$i]);
-	}
-}*/
-t3lib_extMgm::addTypoScriptSetup('plugin.Tx_Formhandler >
-includeLibs.Tx_FormhandlerGui_default = EXT:formhandler/Classes/Controller/tx_FormhandlerGui_Dispatcher.php
-plugin.Tx_Formhandler = USER_INT
-plugin.Tx_Formhandler.userFunc = tx_FormhandlerGui_Dispatcher->main
-tt_content.list.20.formhandler_pi1 < plugin.Tx_Formhandler');
+t3lib_extMgm::addTypoScriptSetup('plugin.tx_formhandler.includeLibs = typo3conf/ext/formhandlergui/pi/class.tx_formhandler.php');
 ?>
---
+++ pi/class.tx_formhandler.php
@@ -0,0 +1,82 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Christian Opitz <co@netzelf.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * 
+ * @package FormhandlerGui
+ * @subpackage Plugin
+ * @author Christian Opitz <co@netzelf.de>
+ * @version $Id: tx_formhandler $
+ * tags
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+require_once (t3lib_extMgm::extPath('formhandler') . 'Classes/Controller/Tx_Formhandler_Dispatcher.php');
+
+/**
+ * This is a clone of class.tx_formhandler.php - we smuggled it in with
+ * 
+ * t3lib_extMgm::addTypoScriptSetup('plugin.tx_formhandler.includeLibs = 
+ * typo3conf/ext/formhandlergui/pi/class.tx_formhandler.php');
+ * 
+ * in ext_tables.php. So we can overwrite all settings for formhandler.
+ *
+ * @author Christian Opitz <co@netzelf.de>
+ * @see typo3conf/ext/formhandler/pi/class.tx_formhandler.php#main()
+ * @see typo3conf/ext/formhandlergui/ext_tables.php#30
+ */
+class tx_formhandler extends Tx_Formhandler_Dispatcher {
+	
+	public $prefixId      = 'tx_formhandler';  // Same as class name
+	
+	public $scriptRelPath = 'pi/class.tx_formhandler.php'; // Path to this script relative to the extension dir.
+	
+	public $extKey        = 'formhandler'; // The extension key.
+	
+	public $pi_checkCHash = true;
+	
+	public $conf;
+	
+	/**
+	 * preloads our setup and calls Tx_Formhandler_Dispatcher::main
+	 *
+	 * @see typo3conf/ext/formhandler/Classes/Controller/Tx_Formhandler_Dispatcher#main()
+	 */
+	public function main($content, $setup) {
+		
+		$setup['settings.']['predef.']['default.']['formValuesPrefix'] = 'formhandlergui';
+		var_dump($setup);
+		
+		$GLOBALS['TSFE']->tmpl->setup['plugin.']['Tx_Formhandler.'] = $setup;
+
+		$result = parent::main($content, $setup);
+		
+		return $result;
+	}
+}
+?>
