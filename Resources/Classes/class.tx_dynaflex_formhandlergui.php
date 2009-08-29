1af5cebd42592c32944533b0a02ea3d510994c58
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-29 21:34:23 +0000

    [ADD] Query in hook for formhandler flexform (fixes #4394)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandlergui/trunk@23772 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Classes/class.tx_dynaflex_formhandlergui.php
+++ Resources/Classes/class.tx_dynaflex_formhandlergui.php
@@ -1,4 +1,33 @@
 <?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009 Christian Opitz <christian.opitz@netzelf.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * Overwrites the dynaflex functions for formhandler plugin.
+ *
+ * $Id$
+ *
+ * @author	Christian Opitz <co@netzelf.de>
+ */
 
 /**
  * Brings predef forms from formhandlergui to formhandler plugin
@@ -21,8 +50,26 @@ class tx_dynaflex_formhandlergui extends tx_dynaflex_formhandler
 	 * @see typo3conf/ext/formhandler/Resources/PHP/tx_dynaflex_formhandler#loadTS($pageUid)
 	 */
 	public function loadTS($pageUid) {
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'uid, title, multiple',
+			'tx_formhandlergui_forms',
+			'deleted = 0 AND hidden = 0',
+			'',
+			'title'
+		);
+		
 		$ts = parent::loadTS($pageUid);
-		$ts['plugin.']['Tx_Formhandler.']['settings.']['predef.']['test.']['name'] = 'Hallo';
+		
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			$id = 'tx_formhandlergui'.$row['uid'].'.';
+			$name = $row['title'];
+			if ($row['multiple']) {
+				$name .= ' (MultiStep)';
+			}
+			
+			$ts['plugin.']['Tx_Formhandler.']['settings.']['predef.'][$id]['name'] = $name;
+		}
+		
 		return $ts;
 	}
 }
