90632afb1b83aca1cbfff6f542b900a2b3c6c1ee
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-29 15:26:45 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandlergui/trunk@23768 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi/class.tx_formhandler.php
+++ pi/class.tx_formhandler.php
@@ -1,37 +1,24 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2009 Christian Opitz <co@netzelf.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 2 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
 
 /**
  * 
+ *
  * @package FormhandlerGui
  * @subpackage Plugin
  * @author Christian Opitz <co@netzelf.de>
  * @version $Id$
- * tags
  */
 
 require_once(PATH_tslib.'class.tslib_pibase.php');
